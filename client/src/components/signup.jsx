30e466eac92d005b0452206f0bd9479e5f5ec745
Danielle Korn <daniella.korn@gmail.com>
2020-06-09 17:14:12 +0300

    signup form

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -4,24 +4,27 @@ import Button from "react-bootstrap/Button";
 
 const SignUp = (props) => {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, useRole] = useState("");
 
   return (
     <Form>
       <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+
+      <Form.Group controlId="formRole">
+        <Form.Control type="text" placeholder="Role" />
       </Form.Group>
+
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Agree to terms and conditions" />
+      </Form.Group>
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
